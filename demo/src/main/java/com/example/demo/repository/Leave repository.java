d23d62e535c278850575d9be837da3ad4c6b3698
Karthick subramanian <karthicksubbiramaniyan@gmail.com>
2024-10-25 21:03:07 +0530

    Delete demo/src/main/java/com/example/demo/repository directory

--- demo/src/main/java/com/example/demo/repository/Leave repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.leaveapp.repository;
-
-import com.example.leaveapp.model.LeaveApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeaveRepository extends JpaRepository<LeaveApplication, Long> {
-}
